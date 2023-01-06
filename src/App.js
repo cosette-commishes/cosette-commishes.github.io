cff264856f0ea0bc99196bc6ace243a418d6bfa6
Nicolás Espejo <nespejobern@uniminuto.edu.co>
2023-01-06 16:19:39 -0500

    header added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter, Router, Route, Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Faq from "./components/Faq";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return(
+        <Container>
+            <BrowserRouter basename="/cosette-commishes">
+                <div>  
+                    <Header />
+                    <Route path="/" exact component={Home} />
+                    <Route path="/faq" exact component={Faq} />
+                </div>
+            </BrowserRouter>
+        </Container>
+    );
 }
 
 export default App;
