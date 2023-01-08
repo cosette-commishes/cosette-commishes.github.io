101d24cc7fd8516dd8da04403d35039fb21286d8
Nicolás Espejo <nespejobern@uniminuto.edu.co>
2023-01-07 23:23:33 -0500

    working in faq and tos

--- src/App.js
+++ src/App.js
@@ -2,20 +2,29 @@ import React from "react";
 import { BrowserRouter, Router, Route, Link } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
-import Header from "./components/Header";
 import Home from "./components/Home";
 import Faq from "./components/Faq";
+import Pricing from "./components/Pricing";
+import OrderTracking from "./components/OrderTracking";
+import Tos from "./components/Tos";
 
 import './App.css';
+import NavHeader from "./components/NavHeader";
+import FloatingLogo from "./components/FloatingLogo";
 
 const App = () => {
     return(
-        <Container>
+        <div> 
             <BrowserRouter basename="/cosette-commishes">
                 <Route path="/" exact component={Home} />
-                <Route path="/faq" exact component={Faq} />
+                <NavHeader />
+                <Route path="/*" exact component={FloatingLogo} />
+                <Route path="/info-and-faq" exact component={Faq} />
+                <Route path="/pricing" exact component={Pricing} />
+                <Route path="/terms-of-service" exact component={Tos} />
+                <Route path="/order-tracking" exact component={OrderTracking} />
             </BrowserRouter>
-        </Container>
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./App";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+createRoot(document.querySelector('#root')).render(<App />);
