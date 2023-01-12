35a62d50bde5dd410a4eaed938391f35ae7e6d0b
Nicolás Espejo <nespejobern@uniminuto.edu.co>
2023-01-12 15:44:09 -0500

    working on Tos.js responsive

--- src/App.js
+++ src/App.js
@@ -13,29 +13,29 @@ import FloatingLogo from "./components/FloatingLogo";
 import { connect } from "react-redux";
 import { changeRoute } from "./actions";
 import Footer from "./components/Footer";
+import { Row } from "react-bootstrap";
 
 class App extends React.Component {
-    
+
     componentDidMount() {
         const path = (window.location.pathname).split('/');
-        this.props.changeRoute("/"+path[ path.length - 1 ]);
+        this.props.changeRoute("/" + path[path.length - 1]);
     }
 
     render() {
         return (
             <div>
-                <BrowserRouter  basename="/cosette-commishes">
+                <BrowserRouter basename="/cosette-commishes">
                     <Route path="/" exact component={Home} />
                     <>
                         {this.props.route === "/" ? null : <NavHeader />}
-                        {this.props.route === "/" ? null : <FloatingLogo />}
                         <Route path="/info-and-faq" exact component={Faq} />
                         <Route path="/pricing" exact component={Pricing} />
                         <Route path="/terms-of-service" exact component={Tos} />
                         <Route path="/order-tracking" exact component={OrderTracking} />
                     </>
                 </BrowserRouter>
-                <Footer></Footer>
+                <Footer />
             </div>
         );
     }
@@ -45,4 +45,4 @@ const mapStateToProps = state => {
     return { route: state.route };
 }
 
-export default connect(mapStateToProps, {changeRoute})(App);
+export default connect(mapStateToProps, { changeRoute })(App);
